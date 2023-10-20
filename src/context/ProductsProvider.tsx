e08db8fdc459295cb51b29005feb42037b33d970
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-10-20 08:48:04 -0400

    Removed all unused vars

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactElement, useState, useEffect } from "react"
+import { createContext, ReactElement, useState } from "react"
 
 export type ProductType = {
     sku: string,
@@ -34,7 +34,7 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState)
 type ChildrenType = { children?: ReactElement | ReactElement[] }
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-    const [products, setProducts] = useState<ProductType[]>(initState)
+    const [products] = useState<ProductType[]>(initState)
 
 
     return (
